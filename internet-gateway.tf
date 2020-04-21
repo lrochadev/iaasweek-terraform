b0203810731a2571c015bc7eeae3dd1ae34ab1dd
Leonardo Rocha <lrochadev@gmail.com>
2020-04-21 10:21:57 -0300

    Creating ecosystem

---
+++ internet-gateway.tf
@@ -0,0 +1,6 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    name = "iaasweek"
+  }
+}
---
+++ subnets.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "private" {
+  count             = var.az_count
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = aws_vpc.main.id
+
+  tags = {
+    Name = "Private"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = var.az_count
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public"
+  }
+}
